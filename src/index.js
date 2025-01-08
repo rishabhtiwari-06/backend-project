c20c2a126287d2c432bc27e7aeb01aa357b6f675
Rishabh Tiwari <rishabhshyam0602@gmail.com>
2025-01-08 22:13:52 +0530

    Utils code

--- src/index.js
+++ src/index.js
@@ -1,37 +1,34 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
-connectDB()
-
-
-
-
-
-
-
-
-
-
-
-
+connectDB()  //It is a function inside db/index.js file
+  .then(() => {
+    app.on("Error", (error) => {
+      console.log("Error", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("DB Connection Failed !!!!", err);
+  });
 
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
-
-
-
-
 
 // import express from "express";
 // const app = express()
 
 // ( async () => {
 //     try {
-//        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`) 
+//        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
 //        app.on("Error",(error)=>{
 //         console.log("Error",error);
 //         throw error
